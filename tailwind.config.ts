f066977c26750d3f923a17a1bfe5b6c986d7faf5
Khomotso <khomotso13@gmail.com>
2024-05-27 14:40:56 +0200

    setup tailwind classes and packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        default: "#FFFFFF",
+        muted: "#FFFFFF",
+        primary: "#FACE8D",
+        inverse: "#081212",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
