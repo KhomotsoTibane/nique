07c02ff991ef5270a7d8cd30126e7030b5aab7f9
Khomotso <khomotso13@gmail.com>
2024-05-30 17:36:43 +0200

    ...

--- app/api/emails/route.ts
+++ app/api/emails/route.ts
@@ -3,7 +3,7 @@ import Booking from "@/emails/Booking"
 import { NextResponse } from 'next/server';
 
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(request:Request) {
 
